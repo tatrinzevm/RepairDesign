bb85ea48bbd6ec7dadac6cb83948e3d07dd2ad21
tatrinzevm <tatrinzevm@gmail.com>
2020-03-05 22:55:04 +0200

    feat: autoprefixer was added to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 function bs() {
   serveSass();
@@ -12,13 +13,16 @@ function bs() {
   watch("./*.html").on('change', browserSync.reload);
   watch("./sass/**/*.sass").on('change', serveSass);
   watch("./js/*.js").on('change', browserSync.reload);
-};
+}
 
 function serveSass() {
   return src("./sass/*.sass")
       .pipe(sass())
+      .pipe(autoprefixer({
+        cascade: false
+      }))
       .pipe(dest("./css"))
       .pipe(browserSync.stream());
-};
+}
 
 exports.serve = bs;
